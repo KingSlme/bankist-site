b33e2383d44c8815a3dde8ffd3931e442da7e24e
KingSlme <djgreene100@gmail.com>
2024-01-27 00:44:26 -0500

    Add smooth scrolling to nav

--- script.js
+++ script.js
@@ -36,3 +36,12 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
