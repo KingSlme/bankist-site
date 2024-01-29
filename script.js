7a0d49935ded010240321dbfd036fd8de5d3bf19
KingSlme <djgreene100@gmail.com>
2024-01-28 19:09:54 -0500

    Add sticky navigation

--- script.js
+++ script.js
@@ -78,3 +78,10 @@ const handleHover = function (e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//Sticky Navigation
+const initialCoords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
