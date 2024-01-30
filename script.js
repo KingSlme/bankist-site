9506f6f024b5e6119d70f927716be67f03f8c763
KingSlme <djgreene100@gmail.com>
2024-01-29 22:45:41 -0500

    Improve sticky nav

--- script.js
+++ script.js
@@ -80,8 +80,23 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 //Sticky Navigation
-const initialCoords = section1.getBoundingClientRect();
+/* const initialCoords = section1.getBoundingClientRect();
 window.addEventListener('scroll', function () {
   if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+}); */
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect();
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
